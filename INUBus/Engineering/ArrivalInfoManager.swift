305511e1784a276d219b0a7a315ed3ce8b0a690d
zunzunzun <zun8ang@gmail.com>
2019-09-09 18:35:10 +0900

    ArrivalInfo 리팩토링

---
+++ INUBus/Engineering/ArrivalInfoManager.swift
@@ -0,0 +1,13 @@
+//
+//  ArrivalInfoManager.swift
+//  INUBus
+//
+//  Created by zun on 08/09/2019.
+//  Copyright © 2019 zun. All rights reserved.
+//
+
+import Foundation
+
+class ArrivalInfoManger {
+  
+}
